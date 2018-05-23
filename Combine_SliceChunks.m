95b054790332d0fc962238c08037bd8820705f30
Chris Klink <p.c.klink@gmail.com>
2018-05-23 11:16:51 +0200

    Fixed some details on recombining slices

--- Combine_SliceChunks.m
+++ Combine_SliceChunks.m
@@ -12,47 +12,47 @@ fld = dir('Slices_*');
 for s=1:length(fld)
     cd(fullfile(fld(s).folder, fld(s).name));
     matfile = ls('*mat');
-    fprintf(['Loading ' matfile '\n']);
+    fprintf(['Loading ' matfile 'Chunk ' num2str(s) '\n']);
     R = load(matfile(1:end-4));
     
     if s==1
         result = R.result;
     else % concat
-        cat(3,result.ang,R.result.ang);
-        cat(3,result.ecc,R.result.ecc);
-        cat(3,result.expt,R.result.expt);
-        cat(3,result.rfsize,R.result.rfsize);
-        cat(3,result.R2,R.result.R2);
-        cat(3,result.gain,R.result.gain);
-        cat(3,result.resnorms,R.result.resnorms);
-        cat(3,result.numiters,R.result.numiters);
-        cat(3,result.meanvol,R.result.meanvol);
+        result.ang      = cat(3,result.ang,R.result.ang);
+        result.ecc      = cat(3,result.ecc,R.result.ecc);
+        result.expt     = cat(3,result.expt,R.result.expt);
+        result.rfsize   = cat(3,result.rfsize,R.result.rfsize);
+        result.R2       = cat(3,result.R2,R.result.R2);
+        result.gain     = cat(3,result.gain,R.result.gain);
+        result.resnorms = cat(3,result.resnorms,R.result.resnorms);
+        result.numiters = cat(3,result.numiters,R.result.numiters);
+        result.meanvol  = cat(3,result.meanvol,R.result.meanvol);
     end
     clear R
 end
 cd(ResFld);
 
 % save the result ----
-fprintf('Saving the result: ');
+fprintf('Merging and saving the result: mat-file, ');
 save(fullfile(ResFld,['pRF_Sess-' Session]),'result','-v7.3');
 
 % also save as nifti files
 % angle ---
-fprintf('Angles ');
+fprintf('Angles, ');
 nii = make_nii(result.ang,[1 1 1],[],[],...
     'pRF fit: Angles (deg)');
 save_nii(nii, fullfile(ResFld, ['Sess-' Session '_ang.nii']));
 gzip(fullfile(ResFld, ['Sess-' Session '_ang.nii']));
 delete(fullfile(ResFld, ['Sess-' Session '_ang.nii']));
 % ecc ---
-fprintf('Ecc ');
+fprintf('Ecc, ');
 nii = make_nii(result.ecc,[1 1 1],[],[],...
     'pRF fit: Eccentricity (pix)');
 save_nii(nii, fullfile(ResFld, ['Sess-' Session '_ecc.nii']));
 gzip(fullfile(ResFld, ['Sess-' Session '_ecc.nii']));
 delete(fullfile(ResFld, ['Sess-' Session '_ecc.nii']));
 % size ---
-fprintf('Size ');
+fprintf('Size, ');
 nii = make_nii(result.rfsize,[1 1 1],[],[],...
     'pRF fit: RF size (pix)');
 save_nii(nii, fullfile(ResFld, ['Sess-' Session '_rfsize.nii']));
