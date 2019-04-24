85b9d5dce6995e562c19a7770b5eff30ebec6151
Chris Klink <p.c.klink@gmail.com>
2019-04-24 11:08:13 +0200

    wip

--- pRF_avg_BOLD_tseries.m
+++ pRF_avg_BOLD_tseries.m
@@ -2,64 +2,70 @@ function pRF_avg_BOLD_tseries(monkey,sess)
 
 %% load the equal length files
 % manual for now
-cd(['pRF_sub-' monkey '_us-avg']);
-load(['ses-' sess '-equal_length']); %#ok<*LOAD>
+cd(['pRF_sub-' monkey '_us-padded']);
+load(['ses-' sess '-230vols']); %#ok<*LOAD>
 
 %% average
 % stim normal of inverted?
 r_fw=[]; r_inv=[];
-for r=1:length(s_run)
-    if ~isempty(s_run(r).stimori)
-        so=s_run(r).stimori(6,2);
+for r=1:length(p_run)
+    if p_run(r).stim{11}(10,80)
+        r_fw=[r_fw r];
     else
-        so=s_run(1).stimori(6,2);
-    end
-    if  so == 90
-        r_fw=[r_fw r]; %#ok<*AGROW>
-    elseif so == 270
         r_inv=[r_inv r];
-    else
-        fprintf('ERROR: unknown stimulus configuration');
     end
+    
+%     if ~isempty(p_run(r).stimori)
+%         so=s_run(r).stimori(6,2);
+%     else
+%         so=s_run(1).stimori(6,2);
+%     end
+%     if  so == 90
+%         r_fw=[r_fw r]; %#ok<*AGROW>
+%     elseif so == 270
+%         r_inv=[r_inv r];
+%     else
+%         fprintf('ERROR: unknown stimulus configuration');
+%     end
 end
 
 %% FW stim
 fprintf('Adressing regular stimuli \n')
 % pre-allocate collection arrays
-run_fw=nan([size(s_run(1).vol{1}) length(s_run(r).vol) length(r_fw)]);
+run_fw=nan([size(p_run(1).vol{1}) length(p_run(r).vol) length(r_fw)]);
 % collect
 for r=r_fw
     fprintf(['Processing r = ' num2str(r) '\n']);
-    nanvol = nan(size(s_run(r).vol{1}));
-    vol = s_run(r).vol;
-    vol(s_run(r).excvol) = {nanvol};
+    nanvol = nan(size(p_run(r).vol{1}));
+    vol = p_run(r).vol;
+    vol(p_run(r).excvol) = {nanvol};
     for t=1:length(vol)
         run_fw(:,:,:,t,r) = vol{t};
     end
 end
 fprintf('Getting the median BOLD signal for all voxels\n');
 medianBOLD = nanmedian(run_fw,5); %#ok<*NASGU>
-stim = s_run(r).stim;
+stim = p_run(r).stim;
 clear run_fw
 
 %% INV stim
 fprintf('Adressing inverse stimuli\n')
 if ~isempty(r_inv)
     % pre-allocate collection arrays
-    run_inv=nan([size(s_run(1).vol{1}) length(s_run(r).vol) length(r_inv)]);
+    run_inv=nan([size(p_run(1).vol{1}) length(p_run(r).vol) length(r_inv)]);
     % collect
     for r=r_inv
         fprintf(['Processing r = ' num2str(r) '\n']);
-        nanvol = nan(size(s_run(r).vol{1}));
-        vol = s_run(r).vol;
-        vol(s_run(r).excvol) = {nanvol};
+        nanvol = nan(size(p_run(r).vol{1}));
+        vol = p_run(r).vol;
+        vol(p_run(r).excvol) = {nanvol};
         for t=1:length(vol)
             run_inv(:,:,:,t,r) = vol{t};
         end
     end
     fprintf('Getting the median BOLD signal for all voxels\n');
     medianBOLD_inv = nanmedian(run_inv,5);
-    stim_inv = s_run(r).stim;
+    stim_inv = p_run(r).stim;
     clear run_inv
 else
     fprintf('Does not exist for this session\n')
