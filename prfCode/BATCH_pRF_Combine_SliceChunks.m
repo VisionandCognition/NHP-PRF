b0309e9bc29268e5fc09b93585aa7a5e90822ceb
Chris Klink <p.c.klink@gmail.com>
2019-07-03 19:04:49 +0200

    Auto stash before merge of "master" and "origin/master"

--- prfCode/BATCH_pRF_Combine_SliceChunks.m
+++ prfCode/BATCH_pRF_Combine_SliceChunks.m
@@ -1,5 +1,6 @@
 Ms={'danny','eddy'};
-Ss={'AllSessions-only_avg','AllSessions-avg-even','AllSessions-avg-odd'};
+Ss={'AllSessions-only_avg','AllSessions-avg-even','AllSessions-avg-odd',...
+    'AllSessions-only_avg_DHRF','AllSessions-avg-even_DHRF','AllSessions-avg-odd_DHRF'};
 for m=1:length(Ms)
     fprintf(['MONKEY: ' Ms{m} '\n']);
     for s=1:length(Ss)
