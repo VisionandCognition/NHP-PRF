f1951617076b5042ccb5fae27f6d41f82608d13a
Chris Klink <p.c.klink@gmail.com>
2019-07-26 11:46:08 +0200

    ephys wip

--- prfCode/pRF_CreateParallel4LISA_ephys.m
+++ prfCode/pRF_CreateParallel4LISA_ephys.m
@@ -5,18 +5,18 @@ for i=1:2
         joblist.monkey = M;
         joblist.sessions = {...
             'mua', [];...
+            'lfp', [];...
             }; % SESSION nWorkers
         joblist.instances = {'1','2','3','4','5','6','7','8'}; % 2 digits, leading zero!
     
-    joblist.sessinc     = 1:size(joblist.instances,1);
+    joblist.sessinc     = 1:size(joblist.sessions,1);
     joblist.type        = 'ephys'; % used as label NB! also the folder where data is loaded from
-    joblist.hrf         = 'none'; % 'HRF_monkey' / 'defaultHRF' / 'none'
     joblist.modeltype   = 'css_ephys'; 
     % 'css_hrf' / 'linear_hrf' / 'dog_hrf'
     % 'css_ephys' / 'linear_ephys' / 'dog_ephys'
     
     joblist.xvalmode    = 1; % 0 / 1 / 2
-    joblist.resfld      = 'ephys_cv1_mua';
+    joblist.resfld      = 'ephys_cv1';
     
     parallel_fun_dir    = '$TMPDIR/PRF/'; %$TMPDIR is fast 'scratch' space
     parallel_fun        = 'pRF_FitModel_LISA_ephys';
