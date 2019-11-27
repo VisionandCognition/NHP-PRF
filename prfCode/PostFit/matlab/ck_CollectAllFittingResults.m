d5d12d6e0504d250932605e5a787cec7492d25f2
Chris Klink <p.c.klink@gmail.com>
2019-11-27 17:58:30 +0100

    add functionality

--- prfCode/PostFit/matlab/ck_CollectAllFittingResults.m
+++ prfCode/PostFit/matlab/ck_CollectAllFittingResults.m
@@ -1,53 +1,54 @@
-CV='cv1'; %'cv0'/'cv1'
+CV='cv0'; %'cv0'/'cv1'
+
+Do.MRI = false;
+Do.EPHYS = true;
+Do.COMBINE = true;
 
 %% MRI ====================================================================
-monkeys = {'eddy','danny'};
-
-models = {...
-    ['linhrf_' CV '_dhrf'],['linhrf_' CV '_mhrf'],...
-    ['linhrf_' CV '_dhrf_neggain'],['linhrf_' CV '_mhrf_neggain'],...
-    ['doghrf_' CV '_dhrf'],['doghrf_' CV '_mhrf'],...
-    ['csshrf_' CV '_dhrf'],['csshrf_' CV '_mhrf']};
-
-output = ['AllFits_MRI_' CV];
-
-ck_GetMRI_pRF(monkeys,models,output);
-
+if Do.MRI
+    monkeys = {'eddy','danny'};
+    
+    models = {...
+        ['linhrf_' CV '_dhrf'],['linhrf_' CV '_mhrf'],...
+        ['linhrf_' CV '_dhrf_neggain'],['linhrf_' CV '_mhrf_neggain'],...
+        ['doghrf_' CV '_dhrf'],['doghrf_' CV '_mhrf'],...
+        ['csshrf_' CV '_dhrf'],['csshrf_' CV '_mhrf']};
+    
+    output = ['AllFits_MRI_' CV];
+    
+    ck_GetMRI_pRF(monkeys,models,output);
+end
 
 %% EPHYS ==================================================================
-monkeys = {'lick','aston'};
-
-models = {...
-    ['linear_ephys_' CV],...
-    ['linear_ephys_' CV '_neggain'],...
-    ['dog_ephys_' CV],...
-    ['css_ephys_' CV],...
-    'classicRF'};
-output = ['AllFits_ephys_' CV];
-
-ck_GetEphys_pRF(monkeys,models,output);
-
+if Do.EPHYS
+    monkeys = {'lick','aston'};
+    
+    models = {...
+        ['linear_ephys_' CV],...
+        ['linear_ephys_' CV '_neggain'],...
+        ['dog_ephys_' CV],...
+        ['css_ephys_' CV],...
+        'classicRF'};
+    output = ['AllFits_ephys_' CV];
+    
+    ck_GetEphys_pRF(monkeys,models,output);
+end
 
 %% COMBINE ================================================================
-fitres_path = ...
-    '/Users/chris/Dropbox/CURRENT_PROJECTS/NHP_MRI/Projects/pRF/FitResults/';
-
-% load mri
-fprintf('Loading MRI\n');
-R_MRI = load(fullfile(fitres_path,'MRI','Combined',['AllFits_MRI_' CV]));
-R_MRI = R_MRI.R;
-% load ephys
-fprintf('Loading EPHYS\n');
-R_EPHYS = load(fullfile(fitres_path,'ephys','Combined',['AllFits_ephys_' CV]));
-R_EPHYS = R_EPHYS.R;
-
-% save
-fprintf('Saving results from both modalities\n');
-save(fullfile(fitres_path,'MultiModal',['AllFits_' CV]),'R_MRI','R_EPHYS');
-
-
-%% PERFORM SOME BASIC PROCESSING ON COLLECTED RESULTS =====================
-
-
-
-%% CREATE LARGE TABLES WITH ALL RESULTS ===================================
+if Do.COMBINE
+    fitres_path = ...
+        '/Users/chris/Dropbox/CURRENT_PROJECTS/NHP_MRI/Projects/pRF/FitResults/';
+    
+    % load mri
+    fprintf('Loading MRI\n');
+    R_MRI = load(fullfile(fitres_path,'MRI','Combined',['AllFits_MRI_' CV]));
+    R_MRI = R_MRI.R;
+    % load ephys
+    fprintf('Loading EPHYS\n');
+    R_EPHYS = load(fullfile(fitres_path,'ephys','Combined',['AllFits_ephys_' CV]));
+    R_EPHYS = R_EPHYS.R;
+    
+    % save
+    fprintf('Saving results from both modalities\n');
+    save(fullfile(fitres_path,'MultiModal',['AllFits_' CV]),'R_MRI','R_EPHYS');
+end
